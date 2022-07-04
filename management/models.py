82b9a2e656303946abaaee99bda9f6b710261264
Coumba Camara <coumba.camara75@gmail.com>
2022-07-04 18:55:54 +0200

    bdd structure

--- management/models.py
+++ management/models.py
@@ -1,12 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    pass
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
-class Praticien(models.Model):
- user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Specialites(models.Model):
+    names =  ArrayField(ArrayField(models.CharField(max_length=10, default=list)))
+    praticien = models.ForeignKey(User,on_delete=models.CASCADE)
 
-class Patient(models.Model):
- user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Prestations(models.Model):
+    names = models.CharField(max_length=200)
+    specialite = models.ForeignKey(Specialites, on_delete=models.CASCADE)
+
+
+class Reservations(models.Model):
+   date_reservation = models.DateTimeField('créneau')
+   prestation = models.ForeignKey(Prestations, on_delete=models.CASCADE)
+   patient = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Disponibilite(models.Model):
+    plage_horaire = models.DateField('plage horaire')
+    specialite = models.ForeignKey(Specialites,on_delete=models.CASCADE)
