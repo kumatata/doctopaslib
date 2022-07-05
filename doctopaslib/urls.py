835e64778ec81700c902bcc26018fe18f209d55e
Emeline GARO <emeline.garo@supinternet.fr>
2022-07-05 15:48:19 +0200

    add auth

--- doctopaslib/urls.py
+++ doctopaslib/urls.py
@@ -4,4 +4,6 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('management.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
