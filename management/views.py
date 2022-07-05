f90e9e4bcc0677561c471e72bbabf1639d23798f
Coumba Camara <coumba.camara75@gmail.com>
2022-07-05 16:33:59 +0200

    remove things

--- management/views.py
+++ management/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from .models import Prestations, Specialites
 
-    # template_name = 'home/index.html'
-    # context_object_name = 'latest_specialities_list'
-
-    # def get_queryset(self):
-    #     return Specialites.objects.order_by('praticien')[:5]
 
 def home(self):
     specialities = Specialites.objects.all()
