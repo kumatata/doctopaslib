d20567d268048cd0a54ff503194a223ee30f0717
Coumba Camara <coumba.camara75@gmail.com>
2022-07-05 19:13:25 +0200

    mes rdv

--- management/views.py
+++ management/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from .models import Prestations, Specialites
+from .models import Prestations, Reservations, Specialites
+from django.contrib.auth.decorators import user_passes_test, login_required
+
 
 
 def home(self):
@@ -11,3 +13,17 @@ def home(self):
 def prestations(request,pk):
     prestations = Prestations.objects.filter(specialite__id=pk)
     return render(request, 'home/prestations.html', {'prestations': prestations})
+
+
+def is_praticien(user):
+     return Specialites.objects.filter(praticien__id=user.id).exists()
+
+def is_patient(user):
+    if is_praticien(user) == False:
+     return True
+
+@user_passes_test(is_patient)
+@login_required
+def reservations_patient(request):
+    reservations = Reservations.objects.filter(patient__id=request.user.id)
+    return render(request, 'patient/gestion.html', {'reservations': reservations})
